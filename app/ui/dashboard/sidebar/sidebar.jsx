dfbd4bdc0f9912826cbc669b5ab8297a3ce0dd84
Giovanni Montanari <giovannimontanari3@gmail.com>
2024-02-28 22:19:04 -0300

    first commit

---
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -0,0 +1,74 @@
+import styles from "./sidebar.module.css";
+import MenuLink from "./menuLink/menulink";
+import Image from "next/image";
+import { MdDashboard, FaUserPlus } from "react-icons/md";
+
+const menuItems = [
+  {
+    title: "Pages",
+    list: [
+      {
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Users",
+        path: "/dashboard/users",
+        icon: <MdDashboard />,
+      },
+    ],
+  },
+  {
+    title: "User",
+    list: [
+      {
+        title: "Settings",
+        path: "/settings",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Help",
+        path: "/help",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Logout",
+        path: "/logout",
+        icon: <MdDashboard />,
+      },
+    ],
+  },
+];
+
+const Sidebar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.user}>
+        <Image
+          className={styles.userImage}
+          src="/noavatar.png"
+          alt=""
+          width="50"
+          height="50"
+        />
+        <div className={styles.userDetails}>
+          <span className={styles.username}>Giovanni</span>
+          <span className={styles.userTitle}>Administrator</span>
+        </div>
+      </div>
+      <ul className={styles.list}>
+        {menuItems.map((category) => (
+          <li key={category.title}>
+            <span className={styles.category}>{category.title}</span>
+            {category.list.map((item) => (
+              <MenuLink item={item} key={item.title} />
+            ))}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
