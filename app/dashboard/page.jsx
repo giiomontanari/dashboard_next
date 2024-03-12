773af42048001ef8c64a79839ea844cde106657f
Giovanni Montanari <giovannimontanari3@gmail.com>
2024-03-12 18:50:09 -0300

    fix error board

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,18 @@
+import styles from "../ui/dashboard/dashboard.module.css";
+import Board from "../ui/dashboard/board/board";
+
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.boards}>
+          <Board />
+          <Board />
+          <Board />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
