8dbfe4144fe3183a7205cc0a439b2ea62c34601a
Giovanni Montanari <giovannimontanari3@gmail.com>
2024-03-14 18:26:10 -0300

    finish dashboard layout

--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -1,14 +1,31 @@
 import styles from "./card.module.css";
+import Image from "next/image";
 
 const Card = () => {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
-        <span className={styles.span}>Nome:</span>
-        <p className={styles.name}>Rosa</p>
+        <div className={styles.description}>
+          <span className={styles.title}>Nome:</span>
+          <p className={styles.name}>Rosa</p>
+        </div>
+        <div className={styles.operator}>
+          <Image
+            className={styles.userImage}
+            src="/noavatar.png"
+            alt=""
+            width="25"
+            height="25"
+          />
+        </div>
       </div>
-      <div className={styles.mid}></div>
-      <div className={styles.bottom}></div>
+      <div className={styles.mid}>
+        <span className={styles.title}>occorrência:</span>
+        <p className={styles.name}>Agressão física</p>
+      </div>
+      <div className={styles.bottom}>
+        <p className={styles.time}>há 1 min</p>
+      </div>
     </div>
   );
 };
